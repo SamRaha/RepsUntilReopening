0af2bb5bbe30160c3337ff327cb9be5274262557
Sam Raha <sam.raha@hermesinnovationlab.com>
2021-03-03 09:18:54 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
     return (
         <div className="App">
           <div className="App-header">
-          <Timer dateToExpire = {new Date(2021, 2, 2, 23, 41)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#00F5D4" numberOfImages={1}/>
-          <Timer dateToExpire = {new Date(2021, 2, 2, 23, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#00BBF9" numberOfImages={2}/>
-          <Timer dateToExpire = {new Date(2021, 2, 2, 24, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#FEE440" numberOfImages={6}/>
-          <Timer dateToExpire = {new Date(2021, 2, 2, 25, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#F15BB5" numberOfImages={6}/>
-          <Timer dateToExpire = {new Date(2021, 2, 2, 26, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#9B5DE5" numberOfImages={12}/>
+          <Timer dateToExpire = {new Date(2021, 2, 2, 23, 41)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#00F5D4" numberOfImages={1}  key={"1"}/>
+          <Timer dateToExpire = {new Date(2021, 2, 4, 23, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#00BBF9" numberOfImages={2}  key={"2"}/>
+          <Timer dateToExpire = {new Date(2021, 2, 5, 24, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#FEE440" numberOfImages={6}onlyShowDays={true}  key={"3"}/>
+          <Timer dateToExpire = {new Date(2021, 2, 6, 25, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#F15BB5" numberOfImages={6}onlyShowDays={true}  key={"4"}/>
+          <Timer dateToExpire = {new Date(2021, 2, 7, 26, 56)} message={"WORKOUT WITH A FRIEND IN THE PARK"} backgroundColor="#9B5DE5" numberOfImages={12}onlyShowDays={true}  key={"5"}/>
         </div>
         </div>
     );
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,6 +4,7 @@ import "./Timer.css"
 import muscleMan from "../assets/muscle-man.png"
 
 function Timer (props) {
+console.log("props is", props);
     let picture = []
 
     for (let i = 0; i < props.numberOfImages; i++) {
@@ -15,14 +16,22 @@ const date2 =  props.dateToExpire  - new Date()
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
       // Render a complete state
-      return <Completionist />;
+      return <Completionist key={props.key} />;
     } else {
       // Render a countdown
-      return (
-        <span class="timer">
-          {days} DAYS, {hours} HOURS, {minutes} MINS, {seconds} SECS
-        </span>
-      );
+      if (props.onlyShowDays) {
+          return (
+            <span class="timer">
+            {days} DAYS
+          </span>
+          )
+      } else {
+        return (
+            <span class="timer">
+              {days} DAYS, {hours} HOURS, {minutes} MINS, {seconds} SECS
+            </span>
+          );
+      }
     }
   };
 
